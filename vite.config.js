ff7cb745d61c4e5351c5100b71ab5bd60e82d948
nyambogahezron <hezzyn1@gmail.com>
2025-02-27 10:36:39 +0300

    refactor: clean up and reorganize codebase, remove unused files, and add new components

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import {
+    defineConfig
+} from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
         react(),
+        tailwindcss(),
     ],
+    esbuild: {
+        jsx: 'automatic',
+    },
 });
